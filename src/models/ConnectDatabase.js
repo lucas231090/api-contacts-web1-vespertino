f804a2fc6305968d4ae6511c6bab2a2576ace7ce
souzalucasj <souza.lucasj@gmail.com>
2024-11-13 14:25:38 -0300

    add contact

--- src/models/ConnectDatabase.js
+++ src/models/ConnectDatabase.js
@@ -1,20 +1,20 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 
 const client = mysql.createPool({
-  host: "localhost",
+  host: 'localhost',
   port: 3306,
-  user: "root",
-  password: "admin",
-  database: "contacts",
+  user: 'root',
+  password: 'admin',
+  database: 'contacts',
 });
 
 const testConnection = async () => {
   try {
     const connection = await client.getConnection();
-    console.log("MySQL conectado com sucesso!");
+    console.log('MySQL conectado com sucesso!');
     connection.release();
   } catch (error) {
-    console.error("Erro ao conectar ao banco de dados:", error);
+    console.error('Erro ao conectar ao banco de dados:', error);
     throw error;
   }
 };
@@ -24,7 +24,7 @@ exports.query = async (_query, values) => {
     const [rows, fields] = await client.execute(_query, values);
     return rows;
   } catch (error) {
-    console.error("Database query error:", error);
+    console.error('Database query error:', error);
     throw error;
   }
 };
